ac82f08fdbda40fb887752b7c8eeb76e275654e7
asyks <aaron@aaronsykes.com>
2018-11-26 21:32:39 -0800

    Add external request async request test client

---
+++ client/async_send.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import asyncio
+import http
+import json
+import sys
+import time
+
+import aiohttp
+
+BASE_URL = "httpbin.org"
+
+ops = ["get", "post", "put", "delete", "head"]
+
+
+async def request(session, url, operation):
+    method = getattr(session, operation)
+    async with method(url) as response:
+        print(response.status)
+        return await response.text()
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        for op in ops:
+            content = await request(
+                session, "http://" + BASE_URL + "/" + op, op
+            )
+            print(content)
+
+
+def do_synchronous():
+    for op in ops:
+        conn = http.client.HTTPConnection(BASE_URL, port=80)
+        conn.request(op.upper(), "/" + op)
+        resp = conn.getresponse()
+        print(resp.status)
+        try:
+            content = json.loads(resp.read())
+        except json.decoder.JSONDecodeError:
+            pass
+        else:
+            print(content)
+
+
+def do_asynchronous():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
+
+if __name__ == "__main__":
+    test = sys.argv[1]
+    start_time = time.time()
+    if test == "sync":
+        do_synchronous()
+    elif test == "async":
+        do_asynchronous()
+    else:
+        print("First arg must be 'sync' or 'async'")
+
+    elapsed_time = time.time() - start_time
+    print(elapsed_time)
